6f02f3d962c6e7ddd170397075a4e985099d9826
masudrahman-dev <masudrahman.dev@gmail>
2023-03-16 00:03:50 +0600

    questions show done

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const loadQuiz = async () => {
   const data = await res.json();
   quizData = data;
   displayQuiz(data);
-  countDown;
+  // countDown();
 };
 
 // Displaying quiz on quiz page
@@ -63,12 +63,13 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
+    // console.log('quizContainer :>> ', quizContainer);
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
